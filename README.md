7585b9a49ac6e9ce54652d3ea78b5b1eb8e9704d
@daaronr <daaronr@gmail.com>
2022-01-20 11:04:10 -0500

    cleanup, adjust main.R to new style, other small tweaks and comments
    @WillemSleegers note that the yaml line
    
    ``` site: bookdown::bookdown_site
    ```
     is necessary to knit on David's machine
    
    testing a second foldable

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # bookdown-template
 The bookdown template used by the survey team of Rethink Priorities.
 
-We are moving towards a template based off the bs4 style. 
-
 We previously used the gitbook template with some tufte elements (as in David Reinstein's template [here](https://github.com/daaronr/dr-rstuff/tree/master/bookdown_template))
+
+We then tried an adaptation of the bs4 style (see earlier commits).
+
+However, we are now back to using the gitbook style, mainly because it offers more flexibility, and does not tie us to having subsection headers on a left-hand-side outline. 
+
 
--- main.R
+++ main.R
@@ -1,36 +1,24 @@
-#### Building the bs4 bookdown ... command line ####
+#### Building the  bookdown ... command line ####
 
 # NOTE: the package loading is redundant for now -- if we use 'David's system' we would remove this from index.Rmd
 
 # Load packages ####
-library(tidyverse)
+library(pacman)
+p_load(dplyr, knitr, bookdown, rmarkdown, bettertrace, install = FALSE)
 
 knitr::write_bib(
   x = c(.packages(), "bookdown", "knitr", "rmarkdown"),
   file = "assets/bib/packages.bib"
 )
 
-
-# Some things that are loaded in most of our actual work, commented out for now
-
-#library(here)
-#library(pacman)
-#here <- here::here()
-
-#source(here("code", "packages.R")) # Install and load packages used in build and analysis (note: these could be cleaned)
-#p_load(devtools)
-#source_url("https://raw.githubusercontent.com/daaronr/dr-rstuff/master/functions/baseoptions.R")
-#source_url("https://raw.githubusercontent.com/daaronr/dr-rstuff/master/functions/plotting_functions.R")
-##--PW_end
-
-# 'switch on or off content'
-#d_anim <-
-#  readline("Do you want animations? They take a long time... (Y/N)")
+# For a list of 'rethink packages' and common packages we install, see: LINK HERE ####
 
 
 # The actual knit/build command ####
 {
   options(knitr.duplicate.label = "allow")
-  rmarkdown::render_site(output_format = 'bookdown::bs4_book', encoding = 'UTF-8')
+  rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
 }
 
+
+# for bs4 style -- rmarkdown::render_site(output_format = 'bookdown::bs4_book', encoding = 'UTF-8')
